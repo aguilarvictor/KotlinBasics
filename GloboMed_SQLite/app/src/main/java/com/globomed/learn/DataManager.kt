83831c5fbff4c28c8f39bc3c1e6b02cf1f7158fb
Victor Aguilar <victor.iag1401@gmail.com>
2022-02-11 20:44:23 -0600

    AllEmployee data fetched from DB and Cursor adapted

---
+++ GloboMed_SQLite/app/src/main/java/com/globomed/learn/DataManager.kt
@@ -0,0 +1,64 @@
+package com.globomed.learn
+
+import com.globomed.learn.GloboMedDbContract.EmployeeEntry
+
+object DataManager {
+    fun fetchAllEmployees(databaseHelper: DatabaseHelper): ArrayList<Employee>{
+        val employees = ArrayList<Employee>()
+
+        /*
+        * Open database connection
+        */
+        val db = databaseHelper.readableDatabase
+
+        /*
+        * Create the array of column names
+        */
+        val columns = arrayOf(
+            EmployeeEntry.COLUMN_ID,
+            EmployeeEntry.COLUMN_NAME,
+            EmployeeEntry.COLUMN_DOB,
+            EmployeeEntry.COLUMN_DESIGNATION
+        )
+
+        /*
+        * Call query() function to fetch the data
+        */
+        val cursor = db.query(
+            EmployeeEntry.TABLE_NAME,
+            columns,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        /*
+        * Fetch the position of the columns
+        */
+        val idPos = cursor.getColumnIndex(EmployeeEntry.COLUMN_ID)
+        val namePos = cursor.getColumnIndex(EmployeeEntry.COLUMN_NAME)
+        val dobPos = cursor.getColumnIndex(EmployeeEntry.COLUMN_DOB)
+        val designationPos = cursor.getColumnIndex(EmployeeEntry.COLUMN_DESIGNATION)
+
+        /*
+        * Cursor initial position is -1. With moveToNext it will be moved to first position.
+        * Iterate to each row, while the cursor can move to a next position
+        * until the end of the cursor is reached.
+        *
+        * Then, fetch the column values and add to employee array
+        */
+        while (cursor.moveToNext()){
+            val id = cursor.getString(idPos)
+            val name = cursor.getString(namePos)
+            val dob = cursor.getLong(dobPos)
+            val designation = cursor.getString(designationPos)
+
+            employees.add(Employee(id, name, dob, designation))
+        }
+
+        cursor.close()
+        return employees
+    }
+}
--- GloboMed_SQLite/app/src/main/java/com/globomed/learn/GloboMedDBContract.kt
+++ GloboMed_SQLite/app/src/main/java/com/globomed/learn/GloboMedDBContract.kt
@@ -12,6 +12,7 @@ object GloboMedDbContract {
     */
     object EmployeeEntry : BaseColumns {
         const val TABLE_NAME = "employee"
+        const val COLUMN_ID = _ID
         const val COLUMN_NAME = "name"
         const val COLUMN_DOB = "dob"
         const val COLUMN_DESIGNATION = "designation"
@@ -21,7 +22,7 @@ object GloboMedDbContract {
 */
         const val SQL_CREATE_ENTRIES: String =
             "CREATE TABLE ${TABLE_NAME} (" +
-                    _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "${COLUMN_NAME} TEXT NOT NULL, " +
                     "${COLUMN_DOB} INTEGER NOT NULL, " +
                     "${COLUMN_DESIGNATION} TEXT NOT NULL)"
