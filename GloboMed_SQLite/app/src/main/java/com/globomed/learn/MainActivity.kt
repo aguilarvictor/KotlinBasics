a6bec73d3d8f756aadb5bd887712eb2123c63a69
Victor Aguilar <victor.iag1401@gmail.com>
2022-02-21 21:01:41 -0600

    RecyclerView added. Database can be displayed in Main Activity

--- GloboMed_SQLite/app/src/main/java/com/globomed/learn/MainActivity.kt
+++ GloboMed_SQLite/app/src/main/java/com/globomed/learn/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.globomed.learn
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    /* First define the RecyclerView adapter */
+    private val employeeListAdapter = EmployeeListAdapter(this)
+    lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        databaseHelper = DatabaseHelper(this)
+        recyclerView.adapter = employeeListAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        employeeListAdapter.setEmployees(DataManager.fetchAllEmployees(databaseHelper))
 
         /*
         * onClicking the Floating action button will be
@@ -23,5 +34,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK){
+            employeeListAdapter.setEmployees(DataManager.fetchAllEmployees(databaseHelper))
+        }
     }
 }
